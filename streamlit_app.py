18c480bfa13c88696c5f5c8dc8510df24d475012
Mikhail Kostin <1bitt2@mail.ru>
2022-06-02 21:22:19 +0300

    add model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import pickle
-import pandas as pd
+import urllib.request
+from gensim.utils import simple_preprocess
+import fasttext
 
 with st.sidebar.expander("Project Goals"):
     """
@@ -11,3 +11,14 @@ with st.sidebar.expander("Project Goals"):
 
 title = st.text_input('Ввести описание')
 st.write('И оно окажется тут ->', title)
+
+url = 'https://github.com/Sekai-no-uragawa/aihack/releases/download/aihack/FastText_baseline.fasttext_model'
+filename = url.split('/')[-1]
+
+urllib.request.urlretrieve(url, filename)
+
+
+
+model = fasttext.load_model(filename)
+text_preproc = ' '.join(simple_preprocess(title))
+model.predict(text_preproc)[0][0][9:]
