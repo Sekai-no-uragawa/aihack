35237c936968b1abe0378bcd72f202ae5bee8ff2
Mikhail Kostin <1bitt2@mail.ru>
2022-06-04 15:27:10 +0300

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,17 +21,17 @@ st.set_page_config(
     layout="wide",
 )
 
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format1 = workbook.add_format({'num_format': '0.000'}) 
-    worksheet.set_column('A:A', None, format1)  
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
+# def to_excel(df):
+#     output = BytesIO()
+#     writer = pd.ExcelWriter(output, engine='xlsxwriter')
+#     df.to_excel(writer, index=False, sheet_name='Sheet1')
+#     workbook = writer.book
+#     worksheet = writer.sheets['Sheet1']
+#     format1 = workbook.add_format({'num_format': '0.000'}) 
+#     worksheet.set_column('A:A', None, format1)  
+#     writer.save()
+#     processed_data = output.getvalue()
+#     return processed_data
 
 def download_button(object_to_download, download_filename, button_text):
     """
@@ -207,10 +207,10 @@ def main():
             classifier = load_classifier()
 
             data_to_download = description_predict_from_file(shows, model, preprocessing)
-            df_xlsx = to_excel(data_to_download)
-            st.download_button(label='📥 Download Current Result',
-                                            data=df_xlsx ,
-                                            file_name= 'df_test.xlsx')
+            # df_xlsx = to_excel(data_to_download)
+            # st.download_button(label='📥 Download Current Result',
+            #                                 data=df_xlsx ,
+            #                                 file_name= 'df_test.xlsx')
             # CSVButton = download_button(
             #     data_to_download,
             #     "File.csv",
