f209d1c62d0c7847e3fbf437e9ad218a649c1a7f
Mikhail Kostin <1bitt2@mail.ru>
2022-06-04 15:02:43 +0300

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,28 +19,6 @@ st.set_page_config(
     layout="wide",
 )
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-
-
 def download_button(object_to_download, download_filename, button_text):
     """
     Generates a link to download the given object_to_download.
@@ -172,8 +150,6 @@ def preprocessing(x):
         return []
 
 def main():
-    set_png_as_page_bg('background.png')
-    
     
     with st.sidebar.expander("Info"):
         """
